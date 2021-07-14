a13e19ef3e9e2a1be840900af5751f0add35e2e6
gbbgarcia <87420386+gbbgarcia@users.noreply.github.com>
2021-07-14 10:54:54 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+# deliberate an element for the matrix
+# utilize gb as the name of the matrix
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  gb <- NULL
+  set <- function(y) {
+    x <<- y
+    gb <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) gb <<- solve
+  getsolve <- function() gb
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
-
-
-## Write a short comment describing this function
-
+## procuring the opposite value
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  gb <- x$getsolve()
+  if(!is.null(gb)) {
+    message("getting inversed matrix")
+    return(gb)
+  }
+  data <- x$get()
+  gb <- solve(data, ...)
+  x$setsolve(gb)
+  gb
+## go back to matrix gb, which is the antithetical of gb
 }
